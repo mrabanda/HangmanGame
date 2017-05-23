04c68e6d8105cf5adc6bf94408e65ef1fda1ed91
mrabanda <mrabanda@gmail.com>
2017-05-23 00:22:57 -0400

    prevent non-letter characters from being entered

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,106 +1,107 @@
-var wordBank = ["PARIS", "LONDON", "SYDNEY", "JOHANNESBURG", "XIANGHAI", "HIROSHIMA", "LAUSANNE", "DUSSELDORF", "ALEXANDRIA", "JERUSALEM", "ISTANBUL", "AMSTERDAM", "BUDAPEST", "SINGAPORE", "MONTREAL", "TRONTO", "MUMBAI", "SHANGHAI", "BANGKOK", "COPENHAGEN", "NAIROBI"];
+var wordBank = ["PARIS", "LONDON", "SYDNEY", "SAN FRANCISCO", "JOHANNESBURG", "XIANGHAI", "HIROSHIMA", "LAUSANNE", "DUSSELDORF", "ALEXANDRIA", "JERUSALEM", "ISTANBUL", "AMSTERDAM", "BUDAPEST", "SINGAPORE", "MONTREAL", "TRONTO", "MUMBAI", "SHANGHAI", "BANGKOK", "COPENHAGEN", "NAIROBI"];
 
 //Stores random word
-var thewrd;
+var theWord;
 
 //Stores word in array
-var thewrdarr = [];
+var theWordarr = [];
 
 //Stores letters already guessed
-var letges = [];
+var letterGuess = [];
 
 //Stores user input from key event
-var usrinp;
+var userInput;
 
 //COUNTER VARIABLES//
 
 //Counts # of times word is guessed
-var winnum = 0;
+var winNumber = 0;
 //Counts # of guesses remaining
-var gesrem = 10;
+var guessRemaining = 10;
 
 
 // GLOBAL FUNCTIONS //
 
 // Choses Word Randomly From wrdBank Then Pushes '_' To An Array Based On Length Of Word Selected
-var rndwrd = function() {
-	thewrd = wordBank[Math.floor(Math.random () * wordBank.length)];
-	for(var i = 0; i < thewrd.length; i++) {
-		thewrdarr.push("_");
-		document.getElementById("crntwrd").innerHTML = thewrdarr.join(" ");
+var randomWord = function() {
+	theWord = wordBank[Math.floor(Math.random () * wordBank.length)];
+	for(var i = 0; i < theWord.length; i++) {
+		if(theWord[i] === " ") {
+			theWordarr.push("&nbsp;");
+		} else {
+			theWordarr.push("_");
 	};
-	return thewrd
+		document.getElementById("current-word").innerHTML = theWordarr.join("");
+	};
+	return theWord
 };
 
-//Resets variables to default and calls on rndwrd function to choose new word
+//Resets variables to default and calls on randomWord function to choose new word
 var reset = function() {
-	thewrdarr = [];
-	letges = [];
-	gesrem = 10;
-	document.getElementById("gesrem").innerHTML = gesrem;
-	document.getElementById("letges").innerHTML = letges;
-	rndwrd();
+	theWordarr = [];
+	letterGuess = [];
+	guessRemaining = 10;
+	document.getElementById("guess-remaining").innerHTML = guessRemaining;
+	document.getElementById("letter-guess").innerHTML = letterGuess;
+	randomWord();
 };
 
-//
-
-rndwrd();
+randomWord();
 
 document.onkeyup = function(event) {
-
-  // Captures the key press, converts it to uppercase, and saves it to a variable.
-  usrinp = event.key.toUpperCase();
+	//checks if keypress is a letter
+	if ((event.keyCode >= 65 && event.keyCode <= 90)) {
+  	// Captures the key press, converts it to uppercase, and saves it to a variable.
+  	userInput = event.key.toUpperCase();
+	} else {
+		return
+	}
 
  	//Pushes user input to array for guessed letters only if it hasn't been guessed already
 
-	if(letges.indexOf(usrinp) === -1) {
-		letges.push(usrinp);
-		document.getElementById("letges").innerHTML = letges.join(" ");
-	};
+	if(letterGuess.indexOf(userInput) === -1) {
+		letterGuess.push(userInput);
+		document.getElementById("letter-guess").innerHTML = letterGuess.join(" ");
+	} else {
+		return
+	}
 
-	//Checks if user input exists in thewrd and pushes input to the corresponding index in thewrdarr
+	//Checks if user input exists in theWord and pushes input to the corresponding index in theWordarr
 
-	if(thewrd.indexOf(usrinp) >= 0) {
-		for(var j = 0; j < thewrd.length; j++) {
-			if(thewrd[j] === usrinp) {
-				thewrdarr[j] = usrinp;
-				document.getElementById("crntwrd").innerHTML = thewrdarr.join(" ");
+	if(theWord.indexOf(userInput) >= 0) {
+		for(var j = 0; j < theWord.length; j++) {
+			if(theWord[j] === userInput) {
+				theWordarr[j] = userInput;
+				document.getElementById("current-word").innerHTML = theWordarr.join(" ");
 			};
 		};
 
-		// If input doesn't exist in thewrd subtract 1 from guesses remaining
+		// If input doesn't exist in theWord subtract 1 from guesses remaining
 
 	} else {
-		gesrem--;
-		document.getElementById("gesrem").innerHTML = gesrem;
+		guessRemaining--;
+		document.getElementById("guess-remaining").innerHTML = guessRemaining;
 	};
 
-	//Checks if "_" does not exist in thewrdarr (meaning the user has guessed all leteers and wins the game). Add 1 to # of wins. Resets the game.
+	//Checks if "_" does not exist in theWordarr (meaning the user has guessed all leteers and wins the game). Add 1 to # of wins. Resets the game.
 
-	if((thewrdarr.indexOf("_") === -1)) {
-		winnum++;
-		document.getElementById("wins").innerHTML = winnum;
-		document.getElementById("lstwrd").innerHTML = thewrd;
+	if((theWordarr.indexOf("_") === -1)) {
+		winNumber++;
+		document.getElementById("wins").innerHTML = winNumber;
+		document.getElementById("last-word").innerHTML = theWord;
 		reset();
 	};
 
 	//Checks if there are no guesses remaining and resets the game
 
-	if(gesrem === 0) {
+	if(guessRemaining === 0) {
 		reset();
 	};
 
 	//Prints variables to document
 
-	
-	
-	
-
-  return usrinp;
-  return gesrem;
-  return winnum;
-  return letges;
-
-
+  return userInput;
+  return guessRemaining;
+  return winNumber;
+  return letterGuess;
 };
-// };
